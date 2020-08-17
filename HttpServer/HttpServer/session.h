5c32e4d9af0e25b17ca31c1ab4be2f6e3491353c
liuning <93347786@qq.com>
2020-08-17 16:57:53 +0800

    去掉send_lambda

--- HttpServer/HttpServer/session.h
+++ HttpServer/HttpServer/session.h
@@ -25,110 +25,6 @@ namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 
-// This function produces an HTTP response for the given
-// request. The type of the response object depends on the
-// contents of the request, so the interface requires the
-// caller to pass a generic lambda for receiving the response.
-template<
-	class Body, class Allocator, class Send>
-	void
-	handle_request(
-		beast::string_view doc_root,
-		http::request<Body, http::basic_fields<Allocator>>&& req,
-		Send&& send)
-{
-	// Returns a bad request response
-	auto const bad_request =
-		[&req](beast::string_view why)
-	{
-		http::response<http::string_body> res{ http::status::bad_request, req.version() };
-		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-		res.set(http::field::content_type, "text/html");
-		res.keep_alive(req.keep_alive());
-		res.body() = std::string(why);
-		res.prepare_payload();
-		return res;
-	};
-
-	// Returns a not found response
-	auto const not_found =
-		[&req](beast::string_view target)
-	{
-		http::response<http::string_body> res{ http::status::not_found, req.version() };
-		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-		res.set(http::field::content_type, "text/html");
-		res.keep_alive(req.keep_alive());
-		res.body() = "The resource '" + std::string(target) + "' was not found.";
-		res.prepare_payload();
-		return res;
-	};
-
-	// Returns a server error response
-	auto const server_error =
-		[&req](beast::string_view what)
-	{
-		http::response<http::string_body> res{ http::status::internal_server_error, req.version() };
-		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-		res.set(http::field::content_type, "text/html");
-		res.keep_alive(req.keep_alive());
-		res.body() = "An error occurred: '" + std::string(what) + "'";
-		res.prepare_payload();
-		return res;
-	};
-
-	// Make sure we can handle the method
-	if (req.method() != http::verb::get &&
-		req.method() != http::verb::head)
-		return send(bad_request("Unknown HTTP-method"), nullptr);
-
-	// Request path must be absolute and not contain "..".
-	if (req.target().empty() ||
-		req.target()[0] != '/' ||
-		req.target().find("..") != beast::string_view::npos)
-		return send(bad_request("Illegal request-target"), nullptr);
-
-	void* pData = nullptr;
-	unsigned long nDataSize = 0;
-	std::string mimeType = "image/jpeg";
-
-	std::shared_ptr<TileProcessor> pRequestProcessor = std::make_shared<TileProcessor>();
-	pRequestProcessor->GetData(std::string(req.target()), &pData, nDataSize, mimeType);
-
-	http::buffer_body::value_type body;
-	body.data = pData;
-	body.size = nDataSize;
-	body.more = false;
-
-	// Respond to HEAD request
-	if (req.method() == http::verb::head)
-	{
-		http::response<http::empty_body> res{ http::status::ok, req.version() };
-		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-		res.set(http::field::content_type, mimeType/*mime_type(path)*/);
-		res.content_length(nDataSize);
-		res.keep_alive(req.keep_alive());
-		return send(std::move(res), nullptr);
-	}
-
-	// Respond to GET request
-	http::response<http::buffer_body> res{
-		std::piecewise_construct,
-		std::make_tuple(std::move(body)),
-		std::make_tuple(http::status::ok, req.version()) };
-	res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-	res.set(http::field::content_type, mimeType/*mime_type(path)*/);
-
-	res.set(http::field::access_control_allow_origin, "*");
-	res.set(http::field::access_control_allow_methods, "POST, GET, OPTIONS, DELETE");
-	res.set(http::field::access_control_allow_credentials, "true");
-
-	res.content_length(nDataSize);
-	res.keep_alive(req.keep_alive());
-	return send(std::move(res), pRequestProcessor);
-}
-
-//------------------------------------------------------------------------------
-
 // Report a failure
 void fail(beast::error_code ec, char const* what);
 
@@ -137,44 +33,131 @@ class session : public std::enable_shared_from_this<session>
 {
 public:
 
-	// This is the C++11 equivalent of a generic lambda.
-	// The function object is used to send an HTTP message.
-	struct send_lambda
-	{
-		session& self_;
-
-		explicit
-			send_lambda(session& self)
-			: self_(self)
-		{
-		}
-
-		template<bool isRequest, class Body, class Fields>
+	// This function produces an HTTP response for the given
+	// request. The type of the response object depends on the
+	// contents of the request, so the interface requires the
+	// caller to pass a generic lambda for receiving the response.
+	template<
+		class Body, class Allocator>
 		void
-			operator()(http::message<isRequest, Body, Fields>&& msg, std::shared_ptr<TileProcessor> requestProcessor) const
+		handle_request(
+			beast::string_view doc_root,
+			http::request<Body, http::basic_fields<Allocator>>&& req)
+	{
+		// Returns a bad request response
+		auto const bad_request =
+			[&req](beast::string_view why)
 		{
-			// The lifetime of the message has to extend
-			// for the duration of the async operation so
-			// we use a shared_ptr to manage it.
-			auto sp = std::make_shared<
-				http::message<isRequest, Body, Fields>>(std::move(msg));
+			http::response<http::string_body> res{ http::status::bad_request, req.version() };
+			res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res.set(http::field::content_type, "text/html");
+			res.keep_alive(req.keep_alive());
+			res.body() = std::string(why);
+			res.prepare_payload();
+			return res;
+		};
 
-			// Store a type-erased version of the shared
-			// pointer in the class to keep it alive.
-			self_.res_ = sp;
+		// Returns a not found response
+		auto const not_found =
+			[&req](beast::string_view target)
+		{
+			http::response<http::string_body> res{ http::status::not_found, req.version() };
+			res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res.set(http::field::content_type, "text/html");
+			res.keep_alive(req.keep_alive());
+			res.body() = "The resource '" + std::string(target) + "' was not found.";
+			res.prepare_payload();
+			return res;
+		};
 
-			self_.requestProcessor_ = requestProcessor;
+		// Returns a server error response
+		auto const server_error =
+			[&req](beast::string_view what)
+		{
+			http::response<http::string_body> res{ http::status::internal_server_error, req.version() };
+			res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res.set(http::field::content_type, "text/html");
+			res.keep_alive(req.keep_alive());
+			res.body() = "An error occurred: '" + std::string(what) + "'";
+			res.prepare_payload();
+			return res;
+		};
 
-			// Write the response
-			http::async_write(
-				self_.stream_,
-				*sp,
-				beast::bind_front_handler(
-					&session::on_write,
-					self_.shared_from_this(),
-					sp->need_eof()));
+		// Make sure we can handle the method
+		if (req.method() != http::verb::get &&
+			req.method() != http::verb::head)
+			return Send(bad_request("Unknown HTTP-method"), nullptr);
+
+		// Request path must be absolute and not contain "..".
+		if (req.target().empty() ||
+			req.target()[0] != '/' ||
+			req.target().find("..") != beast::string_view::npos)
+			return Send(bad_request("Illegal request-target"), nullptr);
+
+		void* pData = nullptr;
+		unsigned long nDataSize = 0;
+		std::string mimeType = "image/jpeg";
+
+		std::shared_ptr<TileProcessor> pRequestProcessor = std::make_shared<TileProcessor>();
+		pRequestProcessor->GetData(std::string(req.target()), &pData, nDataSize, mimeType);
+
+		http::buffer_body::value_type body;
+		body.data = pData;
+		body.size = nDataSize;
+		body.more = false;
+
+		// Respond to HEAD request
+		if (req.method() == http::verb::head)
+		{
+			http::response<http::empty_body> res{ http::status::ok, req.version() };
+			res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res.set(http::field::content_type, mimeType/*mime_type(path)*/);
+			res.content_length(nDataSize);
+			res.keep_alive(req.keep_alive());
+			return Send(std::move(res), nullptr);
 		}
-	};
+
+		// Respond to GET request
+		http::response<http::buffer_body> res{
+			std::piecewise_construct,
+			std::make_tuple(std::move(body)),
+			std::make_tuple(http::status::ok, req.version()) };
+		res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+		res.set(http::field::content_type, mimeType/*mime_type(path)*/);
+
+		res.set(http::field::access_control_allow_origin, "*");
+		res.set(http::field::access_control_allow_methods, "POST, GET, OPTIONS, DELETE");
+		res.set(http::field::access_control_allow_credentials, "true");
+
+		res.content_length(nDataSize);
+		res.keep_alive(req.keep_alive());
+		return Send(std::move(res), pRequestProcessor);
+	}
+
+	template<bool isRequest, class Body, class Fields>
+	void Send(http::message<isRequest, Body, Fields>&& msg, std::shared_ptr<TileProcessor> requestProcessor)
+	{
+		// The lifetime of the message has to extend
+		// for the duration of the async operation so
+		// we use a shared_ptr to manage it.
+		auto sp = std::make_shared<
+			http::message<isRequest, Body, Fields>>(std::move(msg));
+
+		// Store a type-erased version of the shared
+		// pointer in the class to keep it alive.
+		res_ = sp;
+
+		requestProcessor_ = requestProcessor;
+
+		// Write the response
+		http::async_write(
+			stream_,
+			*sp,
+			beast::bind_front_handler(
+				&session::on_write,
+				shared_from_this(),
+				sp->need_eof()));
+	}
 
 	// Take ownership of the stream
 	session(
@@ -182,7 +165,6 @@ public:
 		std::shared_ptr<std::string const> const& doc_root)
 		: stream_(std::move(socket))
 		, doc_root_(doc_root)
-		, lambda_(*this)
 	{
 	}
 
@@ -232,7 +214,7 @@ public:
 			return fail(ec, "read");
 
 		// Send the response
-		handle_request(*doc_root_, std::move(req_), lambda_);
+		handle_request(*doc_root_, std::move(req_));
 	}
 
 	void
@@ -255,6 +237,7 @@ public:
 
 		// We're done with the response so delete it
 		res_ = nullptr;
+		requestProcessor_ = nullptr;
 
 		// Read another request
 		do_read();
@@ -278,7 +261,6 @@ private:
 	http::request<http::string_body> req_;
 	std::shared_ptr<void> res_;
 	std::shared_ptr<void> requestProcessor_;
-	send_lambda lambda_;
 };
 
 #endif //HTTPSERVER_SESSION_H_
