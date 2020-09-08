d8754baf1a0c494e84a9f72af3fad27b73e67f8d
liuning <93347786@qq.com>
2020-09-08 11:04:32 +0800

    添加数据目录支持

--- HttpServer/PieImageServer/main.cpp
+++ HttpServer/PieImageServer/main.cpp
@@ -7,141 +7,22 @@
 // Official repository: https://github.com/boostorg/beast
 //
 
-//------------------------------------------------------------------------------
-//
-// Example: HTTP server, asynchronous
-//
-//------------------------------------------------------------------------------
-
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/config.hpp>
-#include <algorithm>
-#include <cstdlib>
-#include <functional>
 #include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
-
-
-#include "gdal_priv.h"
-#include "listener.h"
-
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
-
-// Return a reasonable mime type based on the extension of a file.
-beast::string_view
-mime_type(beast::string_view path)
-{
-    using beast::iequals;
-    auto const ext = [&path]
-    {
-        auto const pos = path.rfind(".");
-        if (pos == beast::string_view::npos)
-            return beast::string_view{};
-        return path.substr(pos);
-    }();
-    if (iequals(ext, ".htm"))  return "text/html";
-    if (iequals(ext, ".html")) return "text/html";
-    if (iequals(ext, ".php"))  return "text/html";
-    if (iequals(ext, ".css"))  return "text/css";
-    if (iequals(ext, ".txt"))  return "text/plain";
-    if (iequals(ext, ".js"))   return "application/javascript";
-    if (iequals(ext, ".json")) return "application/json";
-    if (iequals(ext, ".xml"))  return "application/xml";
-    if (iequals(ext, ".swf"))  return "application/x-shockwave-flash";
-    if (iequals(ext, ".flv"))  return "video/x-flv";
-    if (iequals(ext, ".png"))  return "image/png";
-    if (iequals(ext, ".jpe"))  return "image/jpeg";
-    if (iequals(ext, ".jpeg")) return "image/jpeg";
-    if (iequals(ext, ".jpg"))  return "image/jpeg";
-    if (iequals(ext, ".gif"))  return "image/gif";
-    if (iequals(ext, ".bmp"))  return "image/bmp";
-    if (iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
-    if (iequals(ext, ".tiff")) return "image/tiff";
-    if (iequals(ext, ".tif"))  return "image/tiff";
-    if (iequals(ext, ".svg"))  return "image/svg+xml";
-    if (iequals(ext, ".svgz")) return "image/svg+xml";
-    return "application/text";
-}
-
-// Append an HTTP rel-path to a local filesystem path.
-// The returned path is normalized for the platform.
-std::string
-path_cat(
-    beast::string_view base,
-    beast::string_view path)
-{
-    if (base.empty())
-        return std::string(path);
-    std::string result(base);
-#ifdef BOOST_MSVC
-    char constexpr path_separator = '\\';
-    if (result.back() == path_separator)
-        result.resize(result.size() - 1);
-    result.append(path.data(), path.size());
-    for (auto& c : result)
-        if (c == '/')
-            c = path_separator;
-#else
-    char constexpr path_separator = '/';
-    if (result.back() == path_separator)
-        result.resize(result.size() - 1);
-    result.append(path.data(), path.size());
-#endif
-    return result;
-}
-
-//------------------------------------------------------------------------------
-
-
-//------------------------------------------------------------------------------
+#include "application.h"
 
 int main(int argc, char* argv[])
 {
-    GDALAllRegister();
+	if (argc != 5)
+	{
+		std::cerr <<
+			"Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
+			"Example:\n" <<
+			"    http-server-async 0.0.0.0 8080 . 1\n";
+		return -1;
+	}
 
-    // Check command line arguments.
-    if (argc != 5)
-    {
-        std::cerr <<
-            "Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
-            "Example:\n" <<
-            "    http-server-async 0.0.0.0 8080 . 1\n";
-        return EXIT_FAILURE;
-    }
-    auto const address = net::ip::make_address(argv[1]);
-    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-    auto const doc_root = std::make_shared<std::string>(argv[3]);
-    auto const threads = std::max<int>(1, std::atoi(argv[4]));
+	global_app = std::make_shared<Application>(argc, argv);
+	global_app->Run();
 
-    // The io_context is required for all I/O
-    net::io_context ioc{ threads };
-
-    // Create and launch a listening port
-    std::make_shared<Listener>(
-        ioc,
-        tcp::endpoint{ address, port },
-        doc_root)->run();
-
-    // Run the I/O service on the requested number of threads
-    std::vector<std::thread> v;
-    v.reserve(threads - 1);
-    for (auto i = threads - 1; i > 0; --i)
-        v.emplace_back(
-            [&ioc]
-            {
-                ioc.run();
-            });
-    ioc.run();
-
-    return EXIT_SUCCESS;
+	return 0;
 }
