9a9735424a80e9dcecfbecb538da17779739ece5
asdln <93347786@qq.com>
2021-08-17 14:20:39 +0800

    屏蔽错误提示

--- HttpServer/PieImageServer/session.cpp
+++ HttpServer/PieImageServer/session.cpp
@@ -8,7 +8,7 @@
 // Report a failure
 void fail(beast::error_code ec, char const* what)
 {
-	std::cerr << what << ": " << ec.message() << "\n";
+	//std::cerr << what << ": " << ec.message() << "\n";
 }
 
 // Take ownership of the stream
